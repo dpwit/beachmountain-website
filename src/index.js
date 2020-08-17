49c03fe9028fdcbbd191b3be4d3663bdb2fb9845
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-08-17 21:29:48 +0100

    Added first basic to do list

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const element = React.createElement('h1', null, 'Beach Mountain');
+//const element = React.createElement('h1', null, 'Beach Mountain');
+
+const element = React.createElement('ol', null,
+	React.createElement('li', null, 'Create to do list'),
+	React.createElement('li', null, 'Save work'),
+	React.createElement('li', null, 'Commit to GitHub'),
+	);
+
+//ReactDom.render (element, document.getElementById('root'));
 
 ReactDom.render (element, document.getElementById('root'));
