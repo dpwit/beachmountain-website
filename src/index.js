5a7dd08ce2f0125cce8c3927280dd0aa082e592b
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-11-03 20:47:47 +0000

    Put components into individual js files

--- src/index.js
+++ src/index.js
@@ -1,33 +1,5 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDom from 'react-dom';
-
-class List extends Component {
-	render() {
-		return (
-			<ol>
-				{ this.props.todo.map((todo, index) => <li key = {index}> {todo} </li>)}
-			</ol>
-		)
-	}
-}
-
-class Title extends Component {
-	render() {
-		return (
-			<h1>{ this.props.title}</h1>
-			)
-	}
-}
-
-class Main extends Component {
-	render() {
-		return	<div>
-							<Title title = {['DPW To do list']}/>
-							<List todo = {['Do the work', 'Check the work']}/>
-							<List todo = {['Save work', 'Commit to GitHub']}/>
-						</div>
-
-	}
-}
+import Main from './Components/Main'
 
 ReactDom.render(<Main/>, document.getElementById('root'));
