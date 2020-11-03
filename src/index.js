4c902366a72c494b5368233738e5e0d994b27a40
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-11-03 20:17:44 +0000

    Added first React components Title and List

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 
-const todo = ['Create to do list', 'to the work', 'Save work', 'Commit to Github'];
-
 class List extends Component {
 	render() {
 		return (
 			<ol>
-				{ todo.map((todo, index) => <li key = {index}> {todo} </li>)}
+				{ this.props.todo.map((todo, index) => <li key = {index}> {todo} </li>)}
 			</ol>
-			)
+		)
 	}
 }
 
 class Title extends Component {
 	render() {
 		return (
-			<h1>To Do List</h1>
+			<h1>{ this.props.title}</h1>
 			)
 	}
 }
 
 class Main extends Component {
 	render() {
-		return (
-		<>
-			<Title/>
-			<List/>
-		</>
-		)
+		return	<div>
+							<Title title = {['DPW To do list']}/>
+							<List todo = {['Do the work', 'Check the work']}/>
+							<List todo = {['Save work', 'Commit to GitHub']}/>
+						</div>
+
 	}
 }
 
-ReactDom.render (<Main/>, document.getElementById('root'));
+ReactDom.render(<Main/>, document.getElementById('root'));
