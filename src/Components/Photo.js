5e93819376831e8c41a1d174c489903d56dc69ad
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-11-05 14:52:38 +0000

    Commented out the const post variable

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 
 class Photo extends Component {
   render() {
-    const post = this.props.post
+    // const post = this.props.post
     return  <figure class="figure">
 
             </figure>
