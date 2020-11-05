b10a173575106d6c2277d1476936d4cc19259f4e
Darren Whatford <darren.whatford@dpwit.co.uk>
2020-11-05 15:45:52 +0000

    Added content to Photo.js

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react'
 
 class Photo extends Component {
   render() {
-    // const post = this.props.post
+    const post = this.props.post
     return  <figure class="figure">
-
+              <img src={post.imageLink} />
             </figure>
   }
 }
